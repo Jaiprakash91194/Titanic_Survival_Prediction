e235168c763772390c346c94993153e2c25339b0
Jaiprakash91194 <jai91194@gmail.com>
2022-07-14 23:47:03 +0530

    Update FlaskApi_Titanic.py
    Changing parameters in Flask API

--- FlaskApi_Titanic.py
+++ FlaskApi_Titanic.py
@@ -20,7 +20,7 @@ df = pd.read_csv("titanicsurvival.csv")
 df["Age"].fillna(df["Age"].mean(), inplace=True)
 df["Sex"] = df["Sex"].map({"male": 1, "female": 0}).astype(int)
 
-X = df.iloc[:,:-1]
+X = df.iloc[:, :-1]
 Y = df.iloc[:, -1]
 
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
@@ -29,38 +29,39 @@ ss = StandardScaler().fit(x_train)
 app = Flask(__name__)
 Swagger(app)
 
+
 @app.route("/", methods=["Get"])
 def predict_survival():
     """Let's predict Titanic Survival.
     Please provide input of Passenger class,Sex,Age,Fare
     ---
     parameters:
+      - name: Gender
+        in: query
+        type: number
+        required: True
       - name: Passenger
         in: query
         type: number
         required: True
-      - name: Sex
-        in: query
-        type: number
-        required: True        
       - name: Age
         in: query
         type: number
-        required: True        
+        required: True
       - name: Fare
         in: query
         type: number
-        required: True  
+        required: True
     responses:
         200:
             description: output the values
     """
-    passenger_class = request.args.get("Passenger")
-    sex = request.args.get("Sex")
+    gender = request.args.get("Gender")
+    p_class = request.args.get("Passenger")
     age = request.args.get("Age")
     fare = request.args.get("Fare")
-    
-    input_values = ss.transform([[passenger_class, sex, age, fare]])
+
+    input_values = ss.transform([[gender, p_class, age, fare]])
     output = rf_model.predict(input_values)[0]
     if output == 1:
         return "The Passenger Survived in Titanic"
